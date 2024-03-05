202aea100eeeebd53615bca24b6e5b46e57917c4
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-03-05 10:29:45 -0500

    added color codes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -11,7 +11,7 @@ function DamageTYPE(){
 const [total, setTotal] = useState(0)
 let [value, setValue] = useState(40) //40 to Win
 let [valueCommander, setValueCommander] = useState(0) //21 to Win
-
+let [poisonCounter,setPoisonCounter]=useState(0)
 
 // Normal
 function Increment(){
@@ -22,6 +22,13 @@ function Decrement(){
     setValue(value--)
 }
 
+function IncrementPoison(){
+    setPoisonCounter(poisonCounter++)
+}
+
+function DecremenPoison(){
+    setPoisonCounter(poisonCounter--)
+}
 
 function DecrementCommander(){
     setValue(value--)
@@ -35,14 +42,14 @@ return(
 
 <input placeholder="Your Name..." type="text"></input>
 
-{damageType ? <button onClick={DamageTYPE}>Commander Damage</button> : <button onClick={DamageTYPE}>Normal Damage</button>}
+{damageType ? <button style={{color:"purple"}} onClick={DamageTYPE}>Commander Damage</button> : <button onClick={DamageTYPE}>Normal Damage</button>}
 
 
 {damageType ? 
-<div>
-<div>Commander Damage</div>
-<button  onClick={Increment}>+</button>
-<button onClick={DecrementCommander}>-</button>
+<div style={{backgroundColor:"rgba(128, 0, 128, 0.39)", padding:"10px"}}>
+<div style={{color:"purple"}}>Commander Damage</div>
+<button style={{color:"purple"}} onClick={Increment}>+</button>
+<button style={{color:"purple"}} onClick={DecrementCommander}>-</button>
 </div>
 :
 <div>
@@ -52,9 +59,15 @@ return(
 </div>
 }
 
+<div style={{backgroundColor:"#00ff2a4d", padding:"10px"}}>
+<div style={{color:"green"}}>Poison Counters</div>
+<button  style={{color:"green"}} onClick={IncrementPoison}>+</button>
+<button  style={{color:"green"}} onClick={DecremenPoison}>-</button>
+</div>
 
-<div>LIFE TOTAL: {value}</div>
-<div>Commander Damage {valueCommander}/21</div>
+<div><strong>LIFE TOTAL: {value}</strong></div>
+<div style={{color:"purple"}}>Commander Damage {valueCommander}/21</div>
+<div style={{color:"green"}}>Poison Counters {poisonCounter}/30</div>
 </div>
 
 )
