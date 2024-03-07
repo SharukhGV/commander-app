0fc34c08f668932071a649c88468c27dbc434e91
Sharukh Velupillai <sharukh.g.velupillai@gmail.com>
2024-03-07 11:41:33 -0500

    repositioned commander counters

--- src/components/ButtonPegs.jsx
+++ src/components/ButtonPegs.jsx
@@ -39,15 +39,8 @@ return(
         <button style={{borderRadius:"10px",backgroundColor:"Green"}} onClick={Green}></button>
         <button style={{borderRadius:"10px",backgroundColor:"Orange"}} onClick={Orange}></button>
         <button style={{borderRadius:"10px",backgroundColor:"Blue"}} onClick={Blue}></button>
-        <button style={{borderRadius:"10px",backgroundColor:"Gray"}} onClick={Grey}></button>
-
-    <input type="text" placeholder="Opponent's Name"/>
-<div style={colors[`${currentColor}`]}>
-    <h2 style={{color:"white"}}>Poison Counters</h2>
-<div><BioHazard /> <BioHazard /> <BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /></div>
-<div><BioHazard /> <BioHazard /> <BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /></div>
-<div><BioHazard /> <BioHazard /> <BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /></div>
-</div>
+        <button style={{borderRadius:"10px",backgroundColor:"Gray"}} onClick={Grey}></button>   
+         <input type="text" placeholder="Opponent's Name"/>
 
 <div style={colors[`${currentColor}`]}>
 <h2 style={{color:"white"}}>Commander Damage</h2>
@@ -56,6 +49,14 @@ return(
 <div><CommanderDamage/><CommanderDamage/><CommanderDamage/><CommanderDamage/><CommanderDamage/><CommanderDamage/><CommanderDamage/></div>
 
 </div>
+<div style={colors[`${currentColor}`]}>
+    <h2 style={{color:"white"}}>Poison Counters</h2>
+<div><BioHazard /> <BioHazard /> <BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /></div>
+<div><BioHazard /> <BioHazard /> <BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /></div>
+<div><BioHazard /> <BioHazard /> <BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /><BioHazard /></div>
+</div>
+
+
 
 </div>
 )
